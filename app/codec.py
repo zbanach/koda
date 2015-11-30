f51f312f2727e63d48a05559f98b80ba28e8a7ad
Zbigniew Banach <z.banach@qsgroup.eu>
2015-11-30 23:18:41 +0100

    Struktura projektu.

---
+++ app/codec.py
@@ -0,0 +1,9 @@
+
+class GolombCoder:
+    def code(source):
+        pass
+
+
+class GolombDecoder:
+    def decode(source):
+        pass
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from utils import entropy
+
+def main():
+    print "Hello"
+    
+
+if __name__ == '__main__':
+    main()
+    
