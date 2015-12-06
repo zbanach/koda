6d701e246b92708d5d9577c925de1445183a61ae
bepawel <bepawel@gmail.com>
2015-12-06 19:22:14 +0100

    Dodanie kodowania obrazów wczytywanych z katalogu oraz testów poprawności kodowania.

---
+++ tests/test_encoding.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from ..app.codec import ExpGolombCodec
+from ..app.utils import entropy, read_image, differential_encoding, differential_decoding, scale_to_positive, scale_to_nonpositive, show_histogram
+from os.path import dirname
+
+def main():
+    IMG_PATH = './images/'
+
+    print "\nTest kodowania obrazów"
+    pictures = ['airplane_bw.png', 'area_bw.png', 'lenna_bw.png', 'pepper_bw.png']
+    for idx, picture in enumerate(pictures, start=1):
+        print "\n{}. Obraz {}".format(idx,picture)
+        data = read_image(IMG_PATH+picture)
+        diffed = differential_encoding(data)
+        scaled = scale_to_positive(diffed)
+        decoded = None
+        for direct in (True, False):
+            codec = ExpGolombCodec(direct)
+            encoded = codec.encode(scaled)
+            decoded = codec.decode(encoded)
+            assert decoded == scaled
+        descaled = scale_to_nonpositive(decoded)
+        assert descaled == diffed
+        dediffed = differential_decoding(descaled)
+        assert dediffed == data
+        print "Obraz wejściowy jednakowy ze zdekodowanym."
+
+if __name__ == '__main__':
+    main()
